e7b5a0e2bd664819bf0aa0ea095a9e7276df00e0
Teng Wei Song <weisong.teng.work@gmail.com>
2020-05-02 15:39:35 +0800

    updated signal r code

--- OneDemo.SignalRChat/Startup.cs
+++ OneDemo.SignalRChat/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OneDemo.SignalRChat.Hubs;
 
 namespace OneDemo.SignalRChat
 {
@@ -24,6 +25,7 @@ namespace OneDemo.SignalRChat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +52,7 @@ namespace OneDemo.SignalRChat
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
